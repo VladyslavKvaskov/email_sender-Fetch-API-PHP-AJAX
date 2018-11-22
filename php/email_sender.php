c692eb3709222fef360133bd7d756a7e2c1cd492
Vladyslav Kvaskov <vladkvaskov@yahoo.com>
2018-11-21 16:29:19 -0800

    Removed unnecessary code

--- php/email_sender.php
+++ php/email_sender.php
@@ -1,16 +1,13 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
-$outp = '';
 
-//get the sql command sent from fetch API
+//get email info from fetch API post request
 $email_message = json_decode($_POST['email_message'], false);
-
 
 $to = $email_message->mail_to;
 $subject = $email_message->mail_subject;
 $txt = $email_message->mail_message;
 //$headers = $email_message->headers->from;
-
 
 if(mail($to,$subject,$txt))
 	echo ('{' . '"email_to":"' . $to . '", "subject":"' . $subject . '", "message":"' . $txt . '", "result" : "success"}');
